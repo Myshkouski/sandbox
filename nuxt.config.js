391c2ecd9a2d804cce215dd47df57e94fb6544b7
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-08-31 14:09:30 +0300

    add Yandex.Metrics head

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 let base
 let publicPath
 
-if(process.env.DEPLOY_ENV == 'GH_PAGES') {
+if (process.env.DEPLOY_ENV == 'GH_PAGES') {
   base = '/sandbox/'
   publicPath = 'https://myshkouski.github.io' + base
 }
@@ -17,5 +17,19 @@ module.exports = {
   },
   router: {
     base
+  },
+  head: {
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        name: 'yandex-verification',
+        content: '72f2c5fa362f4ae3'
+      }
+    ]
   }
 }
