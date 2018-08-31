c5f4c0c11b7e9f4736add65acadde1c28e9f1c6b
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-08-31 11:31:35 +0300

    fixed trailing slash in publicPath

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ module.exports = {
   srcDir: 'src/docs',
   build: {
     parallel: true,
-    publicPath: 'http://myshkouski.github.io/sandbox'
+    publicPath: 'http://myshkouski.github.io/sandbox/'
   },
   generate: {
     dir: 'docs'
