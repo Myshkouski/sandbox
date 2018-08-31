f580d88da1030943c7a05969047b9efe76eb218f
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-08-31 11:52:27 +0300

    add vue router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,15 @@
+const basePath = '/sandbox/'
+
 module.exports = {
   srcDir: 'src/docs',
   build: {
     parallel: true,
-    publicPath: 'https://myshkouski.github.io/sandbox/'
+    publicPath: 'https://myshkouski.github.io' + basePath
   },
   generate: {
     dir: 'docs'
+  },
+  router: {
+    base: basePath
   }
 }
